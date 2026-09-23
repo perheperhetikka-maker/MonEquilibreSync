bc982ea90c199ed9f125229a1b4326c5d4c24af7
perheperhetikka-maker <perheperhe.tikka@gmail.com>
2026-09-23 21:35:45 +0000

    Use Android API 36 for build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.tuuli.monequilibresync"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.tuuli.monequilibresync"
